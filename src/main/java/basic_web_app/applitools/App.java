819363332b9e440844832ca7fcce8a9871535182
arjunkapur95 <arjunkapur95@gmail.com>
2018-05-07 13:34:15 +1000

    all

---
+++ src/main/java/basic_web_app/applitools/App.java
@@ -0,0 +1,18 @@
+package basic_web_app.applitools;
+
+import com.applitools.eyes.TestResults;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.BatchInfo;
+import com.applitools.eyes.RectangleSize;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class App {
+    public static void main(String[] args) throws URISyntaxException {
+    	
+   	}
+}
